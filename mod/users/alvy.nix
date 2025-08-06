6ed5d524b726efa95d75f1123d660c3dcdd55a7e
alvy <alvy@alvy.moe>
2025-08-06 03:15:42 -0500

    balls

---
+++ mod/users/alvy.nix
@@ -0,0 +1,16 @@
+{ lib, config, pkgs, ... }:
+
+{
+  users.users."alvy" = {
+    isNormalUser = true;
+    description = "Alvy";
+    extraGroups = [ "networkmanager" "wheel" "libvertd" ];
+    packages = with pkgs; [
+      kdePackages.kate
+      btop
+      gping
+      ghostty
+      prismlauncher
+    ];
+  };
+}
