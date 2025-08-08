6c3a5a2720bbe6b6f053144a61650bdee1e5fa03
alvy <alvy@alvy.moe>
2025-08-08 11:00:37 -0500

    balls

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,10 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.xserver.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
