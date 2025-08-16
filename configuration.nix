1b4442fc7306eb871bbea73fc858cbe784f016ca
alvy <alvy@alvy.moe>
2025-08-16 06:36:19 -0500

    balls

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,13 @@
     };
   };
 
+  # cache for hyprland and stuff
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
